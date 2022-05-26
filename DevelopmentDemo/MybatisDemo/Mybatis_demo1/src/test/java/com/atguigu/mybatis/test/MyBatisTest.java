42db0a051f11b1e7c30b8683e415b856454d6ba2
slz <88058173@qq.com>
2022-05-26 19:28:58 +0800

    5/25-优化环境配置

--- DevelopmentDemo/MybatisDemo/Mybatis_demo1/src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
+++ DevelopmentDemo/MybatisDemo/Mybatis_demo1/src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
@@ -1,7 +1,9 @@
 package com.atguigu.mybatis.test;
 
+import com.atguigu.mybatis.mapper.ParameterMapper;
 import com.atguigu.mybatis.mapper.UserMapper;
 import com.atguigu.mybatis.pojo.User;
+import com.atguigu.mybatis.utils.SqlSessionUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,7 +12,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
 
@@ -18,20 +22,17 @@ public class MyBatisTest {
     @Test
     public static void main(String[] args) throws IOException {
 
-        //加载核心配置文件
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
-        //获取sqlSessionFactoryBuilder
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        //获取sqlSessionFactory:(生产sqlsession的工厂)
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+
         //获取sqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         //获取mapper接口对象  (获取usermapper实现类对象(实例))
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
 
-        List<User> list = mapper.getUserAll();
+//        User user = new User(null,"李四","123",36,"男","5655@qq.com");
+//        int result = mapper.insertUser(user);
 
-        list.forEach(user -> System.out.println(user));
+        User result = mapper.checkLoginByParam("admin","123456");
+        System.out.println(result);
 
         //提交事务
 //        sqlSession.commit();      (设置自动提交,true)
