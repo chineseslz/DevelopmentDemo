ce597d51096df0335b443861b68d82b16028dfe0
chineseslz <88058173@qq.com>
2022-08-30 17:59:34 +0800

    8-30：Eureka+Nacos+Feign

---
+++ DevelopmentDemo/SpringCloud_Demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,27 @@
+package cn.itcast.order;
+
+import cn.itcast.feign.config.DefaultFeignConfinguration;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+@EnableFeignClients(defaultConfiguration = DefaultFeignConfinguration.class,basePackages = "cn.itcast.feign.client")
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
